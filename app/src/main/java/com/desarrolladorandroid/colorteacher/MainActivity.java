54e5c9c1acc2774c4f3ec60ac60e220adc049458
cosanchez <co.sanchez@mueblesamerica.mx>
2016-03-07 17:49:43 -0600

    first Commit

---
+++ app/src/main/java/com/desarrolladorandroid/colorteacher/MainActivity.java
@@ -0,0 +1,21 @@
+package com.desarrolladorandroid.colorteacher;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.tbMain);
+        setSupportActionBar(toolbar);
+        toolbar.setSubtitle("oks");
+
+    }
+
+
+}
---
+++ app/src/main/java/com/desarrolladorandroid/colorteacher/MainFragment.java
@@ -0,0 +1,24 @@
+package com.desarrolladorandroid.colorteacher;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by cosanchez on 07/03/2016.
+ */
+public class MainFragment extends Fragment {
+    public MainFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+
+
+}
