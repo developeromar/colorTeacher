d1c883cfc9228bfc6fb0ff727acfcbbc084be700
cosanchez <co.sanchez@mueblesamerica.mx>
2016-03-09 17:52:34 -0600

    animation basic

--- app/src/main/java/com/desarrolladorandroid/colorteacher/BasicColors.java
+++ app/src/main/java/com/desarrolladorandroid/colorteacher/BasicColors.java
@@ -1,16 +1,31 @@
 package com.desarrolladorandroid.colorteacher;
 
+import android.annotation.TargetApi;
+import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.desarrolladorandroid.colorteacher.utilities.MainListObject;
 
 public class BasicColors extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_colors);
         MainListObject menu = (MainListObject) getIntent().getExtras().getSerializable("menu");
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setColorFilter(menu.getColor(), PorterDuff.Mode.SRC_IN);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            entrace();
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void entrace() {
+        getWindow().getEnterTransition().setDuration(getResources().getInteger(R.integer.anim_duration_long));
     }
 }
--- app/src/main/java/com/desarrolladorandroid/colorteacher/MainActivity.java
+++ app/src/main/java/com/desarrolladorandroid/colorteacher/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.tbMain);
         setSupportActionBar(toolbar);
 
+
     }
 
 
